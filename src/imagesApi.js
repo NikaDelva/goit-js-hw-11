4feefc8b2861f44a5bc38dbf8f41c67b83915933
NikaDelva <nikadelva@gmail.com>
2023-04-11 16:41:21 +0300

    deleted private #

--- src/imagesApi.js
+++ src/imagesApi.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 export class fetchImages {
-    static #URL = 'https://pixabay.com/api/';
-    static #API_KEY = '35241360-d8dd61788fda51c15adfb22e2';
+    static URL = 'https://pixabay.com/api/';
+    static API_KEY = '35241360-d8dd61788fda51c15adfb22e2';
     static page = 1;
     static query = ''; 
     static per_page = 40;
 
     static fetchPhotosByQuery() {
         const params = new URLSearchParams({
-            key: fetchImages.#API_KEY,
+            key: fetchImages.API_KEY,
             page: fetchImages.page,
             q:  fetchImages.query,
             per_page: fetchImages.per_page,
@@ -17,6 +17,6 @@ export class fetchImages {
             orientation: 'horizontal',
             safesearch: true,
         })
-      return axios.get(`${fetchImages.#URL}?${params}`);
+      return axios.get(`${fetchImages.URL}?${params}`);
     }
 }    
